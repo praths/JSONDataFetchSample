c63672d1769fc16508d384129022a164ffee6bf6
Prathamesh Dabre <prathamesh@alchemytech.ca>
2024-04-27 14:56:12 +0530

    Initial Commit

---
+++ JSONDataFetchSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONDataFetchSample
+//
+//  Created by Prathamesh Dabre on 27/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
